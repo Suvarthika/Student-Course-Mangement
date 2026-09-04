a24259f2e2e92f8fb3edd94b49562fe5fa139df4
Rada Suvarthika <rada.suvarthika@gmail.com>
2026-09-04 17:48:02 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div className="content-panel">
+      <h2>About this application</h2>
+      <p>
+        This course manager is a small React Router and Context API example for
+        student learning.
+      </p>
+    </div>
+  );
+}
+
+export default About;
